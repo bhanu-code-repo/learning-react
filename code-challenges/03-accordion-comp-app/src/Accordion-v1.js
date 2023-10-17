47eb5d4b0181d161029cc8121d25854b1fada331
Bhanu <bpst.web@gmail.com>
2023-10-17 21:42:33 +0100

    code implementation

--- code-challenges/03-accordion-comp-app/src/Accordion-v1.js
+++ code-challenges/03-accordion-comp-app/src/Accordion-v1.js
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import AccordionItem from "./AccordionItem";
 
 function Accordion({ items }) {
-  // declare a new state variable, which we'll call `currentOpen`
-  const [currentOpen, setIsOpen] = useState(0);
-
   return (
     <div className="accordion">
       {items.map((item, index) => (
         <AccordionItem
           key={item.id}
-          currentOpen={currentOpen}
-          onOpen={setIsOpen}
           title={item.title}
           num={index < 9 ? `0${index + 1}` : index + 1}
-        >
-          {<p>{item.text}</p>}
-        </AccordionItem>
+          text={<p>{item.text}</p>}
+        />
       ))}
     </div>
   );
--- code-challenges/03-accordion-comp-app/src/AccordionItem.js
+++ code-challenges/03-accordion-comp-app/src/AccordionItem.js
@@ -1,20 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  function handleToggle() {
-    setIsOpen(!isOpen);
-  }
+function AccordionItem({ num, title, currentOpen, onOpen, children }) {
+  // declare a new variable, which we'll call `isOpen`
+  const isOpen = num === currentOpen;
 
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={() => onOpen(num)}>
       <p className="number">{num}</p>
       <p className="title">{title}</p>
-      <p className="icon" onClick={handleToggle}>
-        {isOpen ? "-" : "+"}
-      </p>
-      {isOpen && <div className="content-box">{text}</div>}
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
